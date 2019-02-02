2f4f0640c65b17312edb7f3467dded0c3cb57072
andreico123 <andreicovaci123@gmail.com>
2019-02-02 08:49:46 +0200

    Added graph class.

--- CanSat/src/DataInterpreter/DI_Main.java
+++ CanSat/src/DataInterpreter/DI_Main.java
@@ -13,8 +13,6 @@ public class DI_Main extends PApplet{
 	//The code below is just for testing purposes
 	
 	private Window window;
-	private Viewport viewportGraph1;
-	private Viewport viewportGraph2;
 	private PApplet p = this;
 
 	private Graph g;
@@ -36,58 +34,28 @@ public class DI_Main extends PApplet{
 		window.setBackColor(255, 255, 255);
 		
 		fraction = 0.00001;
-	    data1 = new double[(int)(100 * (1 / fraction))][2];
+	    data1 = new double[(int)(50 * (1 / fraction))][2];
 	    
 	    for(double i = 0; i * (1 / fraction) < data1.length; i += fraction) {
 	    		data1[(int)(i * (1 / fraction))][0] = i * 10;
 	    		data1[(int)(i * (1 / fraction))][1] = i * (i / 2);
 	    }
 	    
-	    data2 = new double[(int)(100 * (1 / fraction))][2];
+	    data2 = new double[(int)(50 * (1 / fraction))][2];
 	    
 	    for(double i = 0; i * (1 / fraction) < data2.length; i += fraction) {
 	    		data2[(int)(i * (1 / fraction))][0] = i * 5;
 	    		data2[(int)(i * (1 / fraction))][1] = Math.sqrt(i) * 10;
 	    }
 		
-		viewportGraph1 = new Viewport(this, 500, 500, 10, 10);
-		viewportGraph1.setBackColor(255, 0, 0);
-		
-		viewportGraph2 = new Viewport(this, 400, 300, 520, 10);
-		viewportGraph2.setBackColor(255, 0, 0);
-		
-		window.addViewport(viewportGraph1);
-		window.addViewport(viewportGraph2);
+	    int[] a = new int[2];
+	    
+		g = new Graph(10, 10, 500, 500, a, a, data1, fraction, this);
+		window.addViewport(g.getViewport());
 	}
 	
 	public void draw() {
-		viewportGraph1.addDrawAction(new Callable<Integer>() {
-			public Integer call() {
-			    int[] limit = new int[2];
-			    limit[0] = 10;
-			    limit[1] = 10;
-			    
-			    g = new Graph(viewportGraph1.getWidth(), viewportGraph1.getHeight(), limit, limit, data1, fraction,viewportGraph1);
-			    g.drawGraph(p);
-				return 0;
-			}
-		});
-		
-		viewportGraph2.addDrawAction(new Callable<Integer>() {
-			public Integer call() {
-			    int[] limit = new int[2];
-			    limit[0] = 10;
-			    limit[1] = 10;
-			    
-			    g = new Graph(viewportGraph2.getWidth(), viewportGraph2.getHeight(), limit, limit, data2, fraction,viewportGraph2);
-			    g.drawGraph(p);
-				return 0;
-			}
-		});
-		
+		g.drawGraph(p);
 		window.show();
-		
 	}
-	
-	
 }
--- CanSat/src/DataInterpreter/Graph.java
+++ CanSat/src/DataInterpreter/Graph.java
@@ -1,4 +1,6 @@
 package DataInterpreter;
+import java.util.concurrent.Callable;
+
 import GUI.*;
 import processing.core.*;
 
@@ -11,10 +13,13 @@ public class Graph {
 	private double[][] data;
 	double fraction;
 	private Viewport vp;
+	private PApplet parent;
+	
 	private int offset = 10;
 	
-	public Graph(int width, int height, int[] xLimits, int[] yLimits, double[][] data, double fraction,Viewport vp) {
-		this.vp = vp;
+	public Graph(int x, int y, int width, int height, int[] xLimits, int[] yLimits, double[][] data, double fraction, PApplet parent) {
+		this.parent = parent;
+		this.vp = new Viewport(parent, width, height, x, y);
 		this.width = width;
 		this.height = height;
 		this.xLimits = xLimits;
@@ -23,21 +28,30 @@ public class Graph {
 		this.fraction = fraction;
 	}
 	
+	public Viewport getViewport() {
+		return vp;
+	}
+	
 	public void drawGraph(PApplet p) {
-		vp.getObject().fill(255);
-		vp.getObject().rect(0, 0, width, height);
+		vp.addDrawAction(new Callable<Integer>(){
+			public Integer call() {
+				
+				vp.getObject().fill(255);
+				vp.getObject().rect(0, 0, width, height);
+				
+				vp.getObject().rect(offset, offset, 1, height - 2 * offset);
+				vp.getObject().rect(offset, height - offset, width - 2 * offset, 1);
+				
+				PFont font = new PFont();
+				font = p.createFont("Arial", 16);
+				vp.getObject().textFont(font, 36);
+				vp.getObject().fill(0);
+				vp.getObject().text("hello world", 300, 300);
+				//Label l = new Label(p);
+				return 0;
+			}
+		});
 		
-		vp.getObject().rect(offset, offset, 1, height - 2 * offset);
-		vp.getObject().rect(offset, height - offset, width - 2 * offset, 1);
-		
-		/*
-		PFont font = new PFont();
-		font = p.createFont("Arial", 16);
-		p.textFont(font);
-		p.text("hello world", vp.getX() + 10, vp.getY() + 10);
-		Label l = new Label(p);
-		*/
-		//System.out.println(data.length);
 		for(double i = 0; i * (1 / fraction) < data.length; i++) {
 			vp.getObject().rect((int)(offset + data[(int)(i * (1 / fraction))][0]), (int)(vp.getHeight() - offset - data[(int)(i * (1 / fraction))][1]), 1, 1);
 		}
